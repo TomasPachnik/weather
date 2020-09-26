897f36c6c140859102315c96698383bffccf8f77
Tomas Pachnik <tomas.pachnik@sigp.eu>
2020-09-26 23:22:14 +0200

    dynamic values WIP

--- src/main/java/sk/tomas/wm/controller/GraphController.java
+++ src/main/java/sk/tomas/wm/controller/GraphController.java
@@ -2,6 +2,7 @@ package sk.tomas.wm.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -9,9 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/graph")
 public class GraphController {
 
-
     @RequestMapping
-    public String index() {
+    public String index(Model model) {
+        String[] labels = {"pondelok", "utorok", "streda", "štvrtok", "piatok", "sobota", "nedeľa"};
+        double[] temperature = {8.62, 8.02, 13.62, 9.62, 8.62, 12.62, 18.62};
+        double[] humidity = {282, 350, 411, 502, 635, 809, 947};
+        double[] pressure = {168, 170, 178, 190, 203, 276, 408};
+        model.addAttribute("labels", labels);
+        model.addAttribute("temperature", temperature);
+        model.addAttribute("humidity", humidity);
+        model.addAttribute("pressure", pressure);
         return "index.html";
     }
 
