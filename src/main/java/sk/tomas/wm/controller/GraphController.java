1b6866b50a3f011a1340978189dd5fb846b5ae00
Tomas Pachnik <tomas.pachnik@sigp.eu>
2020-10-11 12:23:46 +0200

    new display logic

--- src/main/java/sk/tomas/wm/controller/GraphController.java
+++ src/main/java/sk/tomas/wm/controller/GraphController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sk.tomas.wm.Weather;
 import sk.tomas.wm.dao.WeatherDao;
 import sk.tomas.wm.entity.WeatherEntity;
 
@@ -13,8 +14,10 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -44,55 +47,89 @@ public class GraphController {
         List<Double> humidity = new ArrayList<>();
         List<Double> pressure = new ArrayList<>();
         OffsetDateTime last = OffsetDateTime.MIN;
-        int dividend;
         String time;
         switch (period) {
             case "week":
                 weatherInfo = weatherDao.getDays(6);
-                dividend = 7;
                 time = "Posledný týždeň";
                 break;
             case "threeDays":
                 weatherInfo = weatherDao.getDays(2);
-                dividend = 3;
                 time = "Posledné tri dni";
                 break;
             case "twoDays":
                 weatherInfo = weatherDao.getDays(1);
-                dividend = 2;
                 time = "Posledné dva dni";
                 break;
             case "today":
             default:
                 weatherInfo = weatherDao.getDays(0);
-                dividend = 1;
                 time = "Dnes";
         }
 
-        for (int i = 0; i < weatherInfo.size(); i++) {
+        //get actual weather info and remove it from list
+        WeatherEntity actualWeather = weatherInfo.remove(weatherInfo.size() - 1);
 
-            //if every x entry or last entry
-            if (i % dividend == 0 || (weatherInfo.size() - 1 == i)) {
-                WeatherEntity weather = weatherInfo.get(i);
-                //if next day, show day name instead of time
-                if (weatherInfo.get(i).getCreated().truncatedTo(ChronoUnit.DAYS).isAfter(last.truncatedTo(ChronoUnit.DAYS))) {
-                    last = weather.getCreated();
-                    labels.add(weather.getCreated().format(DAY_FORMAT).toUpperCase());
-                } else {
-                    labels.add(weather.getCreated().format(FORMAT));
-                }
+        Map<OffsetDateTime, List<WeatherEntity>> map = groupByHour(weatherInfo);
 
-                temperature.add(((double) weather.getTemperature()) / 100);
-                humidity.add(((double) weather.getHumidity()) / 100);
-                pressure.add(((double) weather.getPressure()) / 10_000);
+        for (Map.Entry<OffsetDateTime, List<WeatherEntity>> item : map.entrySet()) {
+            Weather weather = average(item.getValue());
+
+            //if next day, show day name instead of time
+            if (item.getKey().truncatedTo(ChronoUnit.DAYS).isAfter(last.truncatedTo(ChronoUnit.DAYS))) {
+                last = item.getKey();
+                labels.add(item.getKey().plusMinutes(30).format(DAY_FORMAT).toUpperCase());
+            } else {
+                labels.add(item.getKey().plusMinutes(30).format(FORMAT));
             }
+
+            temperature.add(((double) weather.getTemperature()) / 100);
+            humidity.add(((double) weather.getHumidity()) / 100);
+            pressure.add(((double) weather.getPressure()) / 10_000);
         }
+
+        labels.add(actualWeather.getCreated().format(FORMAT));
+        temperature.add(((double) actualWeather.getTemperature()) / 100);
+        humidity.add(((double) actualWeather.getHumidity()) / 100);
+        pressure.add(((double) actualWeather.getPressure()) / 10_000);
 
         model.addAttribute("labels", labels.toArray());
         model.addAttribute("temperature", temperature.toArray());
         model.addAttribute("humidity", humidity.toArray());
         model.addAttribute("pressure", pressure.toArray());
         model.addAttribute("time", time);
+    }
+
+    private Map<OffsetDateTime, List<WeatherEntity>> groupByHour(List<WeatherEntity> weatherInfo) {
+        Map<OffsetDateTime, List<WeatherEntity>> result = new LinkedHashMap<>();
+        for (WeatherEntity item : weatherInfo) {
+            OffsetDateTime created = item.getCreated();
+            OffsetDateTime truncated = created.truncatedTo(ChronoUnit.HOURS);
+            if (result.get(truncated) == null) {
+                List<WeatherEntity> list = new ArrayList<>();
+                list.add(item);
+                result.put(truncated, list);
+            } else {
+                result.get(truncated).add(item);
+            }
+        }
+        return result;
+    }
+
+    private Weather average(List<WeatherEntity> list) {
+        Weather result = new Weather();
+
+        for (WeatherEntity item : list) {
+            result.setTemperature(result.getTemperature() + item.getTemperature());
+            result.setPressure(result.getPressure() + item.getPressure());
+            result.setHumidity(result.getHumidity() + item.getHumidity());
+        }
+
+        result.setTemperature(result.getTemperature() / list.size());
+        result.setPressure(result.getPressure() / list.size());
+        result.setHumidity(result.getHumidity() / list.size());
+
+        return result;
     }
 
 }
